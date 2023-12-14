ae0b7f8f7e6289f156ea1e474a34128857fadb21
Frank <0765655151@protonmail.ch>
2023-12-14 15:00:47 +0100

    mise en page

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.post('/traitement-formulaire', async (req, res) => {
   const { nom, prenom, email, message } = req.body;
   await client.query(`
     INSERT INTO contact (first_name, last_name, email, message)
-    VALUES ('John', 'Doe', 'john.doe@example.com', '123-456-7890');
+    VALUES ('John', 'Doe', 'john.doe@example.com', 'Exemple');
   `)
   // Vous pouvez traiter les données du formulaire ici
   res.send(`Données reçues : Nom=${nom}, Prénom=${prenom}, Email=${email}, Message=${message}`);
